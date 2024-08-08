2378b5805d0af92da99ef9e06d122e536c7566a3
王冶 <wangye@22s-MacBook-Pro.local>
2024-08-08 11:36:19 +0800

    d

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -150,7 +150,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
